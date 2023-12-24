a294d6cea1e036a46677bd90814f7f82e86b301d
Mikita <nikitakuza1999@gmail.com>
2023-12-24 19:03:11 +0100

    refactor: improve the code; feat: add find method for binary-search-tree.js

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -13,30 +13,21 @@ class BinarySearchTree {
   }
 
   root() {
-    if (this.tree === undefined) {
-      return null;
-    } else {
-      return this.tree;
-    }
+    if (this.tree === undefined) return null;
+    
+    return this.tree;
   }
 
   add(data) {
     this.tree = addNewRoot(this.tree, data);
 
     function addNewRoot(node, data) {
-      if(!node) {
-        return new Node(data);
-      }
+      if(!node) return new Node(data);
       
-      if(node.data === data) {
-        return node; 
-      }
+      if(node.data === data) return node;
 
-      if(data < node.data) {
-        node.left = addNewRoot(node.left, data);
-      } else {
-        node.right = addNewRoot(node.right, data);
-      }
+      if(data < node.data) node.left = addNewRoot(node.left, data);
+      if(data > node.data) node.right = addNewRoot(node.right, data);
 
       return node;
     }
@@ -62,9 +53,17 @@ class BinarySearchTree {
     }
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(data) {
+    return findNode(this.tree, data);
+    
+    function findNode(node, data) { 
+      if(!node) return null;
+      
+      if(node.data === data) return node;
+     
+      if(data < node.data) return findNode(node.left, data); 
+      if(data > node.data) return findNode(node.right, data);
+    }
   }
 
   remove(/* data */) {
@@ -73,9 +72,7 @@ class BinarySearchTree {
   }
 
   min() {
-    if(!this.tree) {
-      return undefined;
-    }
+    if(!this.tree) return null;
 
     let node = this.tree;
     while(node.left) {
@@ -86,9 +83,7 @@ class BinarySearchTree {
   }
 
   max() {
-    if(!this.tree) {
-      return undefined;
-    }
+    if(!this.tree) return null;
 
     let node = this.tree;
     while(node.right) {
